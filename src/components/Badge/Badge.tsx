8165cafda160e5efb72de6a419f0fe394aa5bdea
guilermmm <guilherme.r.a.melo@gmail.com>
2023-05-25 10:34:28 -0300

    consertando forwardref no badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,45 +1,50 @@
 import { forwardRef } from 'react'
 import { classList } from '../../utils'
 import { BadgeProps } from './types'
+import { CompoundedComponent } from '../../utils/types'
 
-const Badge = forwardRef(
-  ({
-    type = 'primary',
-    badgeFor,
-    bgPosition = 'top-right',
-    ...props
-  }: BadgeProps) => {
-    const canBePositioned =
-      badgeFor == 'buttonDetail' || badgeFor == 'buttonPositioned'
+const InternalBadge: React.ForwardRefRenderFunction<
+  HTMLSpanElement,
+  BadgeProps
+> = (
+  { type = 'primary', badgeFor, bgPosition = 'top-right', ...props },
+  ref,
+) => {
+  const canBePositioned =
+    badgeFor == 'buttonDetail' || badgeFor == 'buttonPositioned'
 
-    return (
+  return (
+    <span
+      ref={ref}
+      {...props}
+      className={classList(` bg-${type}`, {
+        'badge position-absolute translate-middle rounded-pill':
+          badgeFor == 'buttonPositioned',
+        'badge position-absolute translate-middle border border-light rounded-circle p-2':
+          badgeFor == 'buttonDetail',
+        'position-absolute bottom-0 start-75 translate-middle p-1 border border-light rounded-circle':
+          badgeFor == 'icon',
+        'mx-1': badgeFor == 'button',
+        'badge ': badgeFor == 'button' || badgeFor == 'text',
+        'top-0 start-0': bgPosition == 'top-left' && canBePositioned,
+        'top-0 start-100': bgPosition == 'top-right' && canBePositioned,
+        'top-100 start-0': bgPosition == 'bottom-left' && canBePositioned,
+        'top-100 start-100': bgPosition == 'bottom-right' && canBePositioned,
+      })}
+    >
       <span
-        {...props}
-        className={classList(` bg-${type}`, {
-          'badge position-absolute translate-middle rounded-pill':
-            badgeFor == 'buttonPositioned',
-          'badge position-absolute translate-middle border border-light rounded-circle p-2':
-            badgeFor == 'buttonDetail',
-          'position-absolute bottom-0 start-75 translate-middle p-1 border border-light rounded-circle':
-            badgeFor == 'icon',
-          'mx-1': badgeFor == 'button',
-          'badge ': badgeFor == 'button' || badgeFor == 'text',
-          'top-0 start-0': bgPosition == 'top-left' && canBePositioned,
-          'top-0 start-100': bgPosition == 'top-right' && canBePositioned,
-          'top-100 start-0': bgPosition == 'bottom-left' && canBePositioned,
-          'top-100 start-100': bgPosition == 'bottom-right' && canBePositioned,
+        className={classList({
+          'visually-hidden': badgeFor == 'buttonDetail' || badgeFor == 'icon',
         })}
       >
-        <span
-          className={classList({
-            'visually-hidden': badgeFor == 'buttonDetail' || badgeFor == 'icon',
-          })}
-        >
-          {props.children}
-        </span>
+        {props.children}
       </span>
-    )
-  },
-)
+    </span>
+  )
+}
+
+const Badge = forwardRef<HTMLSpanElement, BadgeProps>(
+  InternalBadge,
+) as CompoundedComponent<BadgeProps>
 
 export default Badge
